cc7ed8875d2a6e7ceeb5a68e1c69062c6fc3debe
T-bit <tibit.michelangelo@gmail.com>
2024-09-25 22:22:16 +0300

    Setup Game object

--- Assets/TestEventService/Scripts/Runtime/Game.cs
+++ Assets/TestEventService/Scripts/Runtime/Game.cs
@@ -1,8 +1,13 @@
-﻿using UnityEngine;
+﻿using TestEventService.Attributes;
+using TestEventService.Services;
+using UnityEngine;
 
 namespace TestEventService
 {
-    public sealed class Game : MonoBehaviour
+    public sealed class Game : MonoSingleton<Game>
     {
+        [SerializeReference]
+        [Polymorphic]
+        private IService[] _services = new IService[0];
     }
 }
---
+++ Assets/TestEventService/Scripts/Runtime/MonoSingleton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace TestEventService
+{
+    public abstract class MonoSingleton<T> : MonoBehaviour
+        where T : MonoSingleton<T>
+    {
+        public static T Instance
+        {
+            get;
+            private set;
+        }
+
+        #region Unity
+
+        protected virtual void Awake()
+        {
+            Assert.IsNull(Instance);
+
+            Instance = (T)this;
+        }
+
+        protected virtual void OnDestroy()
+        {
+            Assert.IsNotNull(Instance);
+
+            Instance = default;
+        }
+
+        #endregion
+    }
+}
