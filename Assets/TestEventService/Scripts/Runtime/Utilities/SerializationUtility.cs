2e7d42bea55dfcbbb087dbfedf8809671ee16935
T-bit <tibit.michelangelo@gmail.com>
2024-10-04 20:16:48 +0300

    Added SerializationUtility

---
+++ Assets/TestEventService/Scripts/Runtime/Utilities/SerializationUtility.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using UnityEngine;
+
+namespace TestEventService.Utilities
+{
+    public static class SerializationUtility
+    {
+        private static readonly JsonSerializerSettings Settings = new()
+        {
+            Error = OnError,
+            NullValueHandling = NullValueHandling.Ignore
+        };
+
+        private static void OnError(object sender, ErrorEventArgs errorEventArgs)
+        {
+            var errorContext = errorEventArgs.ErrorContext;
+
+            Debug.LogException(errorContext.Error);
+
+            errorContext.Handled = true;
+        }
+
+        public static string Serialize<T>(T value, bool prettyPrint)
+            where T : class
+        {
+            var formatting = prettyPrint
+                ? Formatting.Indented
+                : Formatting.None;
+
+            return JsonConvert.SerializeObject(value, formatting, Settings);
+        }
+
+        public static T Deserialize<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json, Settings);
+        }
+    }
+}
