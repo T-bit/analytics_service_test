27d4175f1578755e1c0bd3065da3bb08d19baf4c
T-bit <tibit.michelangelo@gmail.com>
2024-10-04 22:11:27 +0300

    Added EventsData

---
+++ Assets/TestEventService/Scripts/Runtime/Events/EventsData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace TestEventService.Events
+{
+    public sealed class EventsData
+    {
+        [JsonProperty("events")]
+        public List<Event> Events = new();
+    }
+}
