40ebb68b3545a075596a0a6d8f42c42db1a757c8
Mikhail Demidov <tibit.michelangelo@gmail.com>
2024-10-04 19:57:09 +0300

    Project setup (#1)
    * Added unity project
    
    * Added Newtonsoft Json, UniTask packages
    
    * Moved assets
    
    * Added some empty scripts
    
    * Added Runtime assembly definition
    
    * Added Editor assembly definition, PolimorphicAttribute
    
    * Setup Game object
    
    * Removed unnecessary files
    
    * Added IEventService

---
+++ Assets/TestEventService/Scripts/Runtime/Game.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using TestEventService.Services;
+using UnityEngine;
+
+namespace TestEventService
+{
+    public sealed class Game : MonoBehaviour
+    {
+        private readonly List<IService> _services = new();
+
+        #region Unity
+
+        private void Awake()
+        {
+            gameObject.GetComponentsInChildren(_services);
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/TestEventService/Scripts/Runtime/Services/IService.cs
@@ -0,0 +1,6 @@
+﻿namespace TestEventService.Services
+{
+    public interface IService
+    {
+    }
+}
