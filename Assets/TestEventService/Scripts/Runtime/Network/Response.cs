7f0fbf6e7517396693254be2b549c6419b98b0f6
T-bit <tibit.michelangelo@gmail.com>
2024-10-04 21:19:07 +0300

    Added Request

---
+++ Assets/TestEventService/Scripts/Runtime/Network/Response.cs
@@ -0,0 +1,19 @@
+﻿using TestEventService.Extensions;
+
+namespace TestEventService.Network
+{
+    public readonly struct Response
+    {
+        private readonly byte[] _bytes;
+
+        public Response(byte[] bytes)
+        {
+            _bytes = bytes;
+        }
+
+        public override string ToString()
+        {
+            return _bytes == null ? string.Empty : _bytes.ToUTF8String();
+        }
+    }
+}
